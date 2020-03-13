b72cee473edfc02ca69888f39888a6f21088fd25
moez-saafi <moez0505@gmail.com>
2020-03-13 11:00:39 +0100

    hi

--- warmUp1.js
+++ warmUp1.js
@@ -1,6 +1,38 @@
 // 1-using + operator combine your partner first and last name .
-// 2-find if the number 13 is a multiple of 3 or not.
-// 3-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
-// 4-calculate your age in seconds.
+function name(first,last){
+	return first +' '+ last;
+}
 
+// 2-find if the number 13 is a multiple of 3 or not.
+function multiple(x,y){
+	var z= x%y;
+	if (z===0){
+		return x +' '+ 'is a multiple of'+' '+ y;
+	}
+	return x +' '+'is not a multiple of'+' '+ y;
+}
+
+// conclusion: "13 is not a multiple of 3"
+
+// 3-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
+
+function average (){
+	var sum= (13+14+13+15+16+17+19+13+16+15)= 151;
+	return sum/arr.length;
+}
+
+function age(array){
+	var average= age[i];
+	for( var i=0;i<array.length;i++){
+		average=(age[i]+age[i+1])/2;
+	}
+	return average;
+}
+
+
+
+// 4-calculate your age in seconds.
+function myage(){
+return ((42*365)+9)*24*60*60;
+}
 // your code is here
