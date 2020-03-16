25800a47ab1e83f317be0586e038963c51fbeab3
moez-saafi <moez0505@gmail.com>
2020-03-16 10:01:11 +0100

    did not finish solving

--- warmUp3.js
+++ warmUp3.js
@@ -1,15 +1,40 @@
-// 1 - Write functions larger and smaller that each accept two strings as arguments, and return the larger and smaller strings, respectively
+// 1 - Write functions larger and smaller that each accept two strings as arguments,
+//and return the larger and smaller strings, respectively
 
-// 2 - Write a function called counter that, when invoked, always returns a number that is one more than the previous invocation. For instance:
-// function counting(n) { 
-    TODO: your code here 
-// } 
+function larger(str1, str2){
+if (str1.length>str2.length){
+	return str1;
+}
+return str2;
+}
+
+function smaller(str1, str2){
+	if (str1.length<str2.length){
+	return str1;
+}
+return str2;
+
+}
+
+// 2 - Write a function called counter that, when invoked, always returns a number
+//that is one more than the previous invocation. For instance:
+
+function counting(n) { 
+    var counter=0;
+    for (var i=0; i<counting.length; i++){
+    	couter= counter+ counting(n-1);
+    }
+    return counter;
+} 
+
+
 // counting(5); // => '1, 2, 3, 4, 5' 
 // counting(1); // => '1' 
 // counting(3); // => '1, 2, 3'
 
 
-// 3 -  Meet & Greet: n numbers are coming to a party for numbers. Introduce them each to all the guests as they arrive. You should end up with output looking something like this one for n = 5
+// 3 -  Meet & Greet: n numbers are coming to a party for numbers. Introduce them each
+// to all the guests as they arrive. You should end up with output looking something like this one for n = 5
 // welcome 1
 //  welcome 2, meet 1
 //  welcome 3, meet 1 and 2
